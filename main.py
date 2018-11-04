6d5184a6bea78ededd8a10d34f60394a46287552
Rhendz <patelankush.2012@gmail.com>
2018-11-04 04:05:36 -0600

    Added POST functionality

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
+import io
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('index.html')
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f = open(f, 'r')
+        print(f.read())
+    else:
+        return render_template('index.html')
