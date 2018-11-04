4e350dcb90be2919c3dde28ef047e6191e863091
Rhendz <patelankush.2012@gmail.com>
2018-11-04 07:44:52 -0600

    Added db

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import urllib.request
+import json
 from flask import Flask, render_template, request
+from google.cloud import datastore
 
 APP_ID = '5cf1fbde'
 API_KEY = 'a1e424815db07a8297e670403ca00a9e'
@@ -7,14 +9,30 @@ API_KEY = 'a1e424815db07a8297e670403ca00a9e'
 app = Flask(__name__)
 
 
+def create_client(project_id):
+    return datastore.Client(project_id)
+
+
+def add_task(client, dateTime, fdata):
+    key = client.key(dateTime)
+
+    task = datastore.Entity(
+        key, exclude_from_indexes=['dateTime'])
+
+    task.update(fdata)
+
+    client.put(task)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
+        client = create_client('tracker-221417')
         f = request.get_json()
 
         EDA_REQUEST = 'https://api.edamam.com/api/food-database/parser' + '?app_id=' + APP_ID + '&app_key=' + API_KEY + '&ingr=' + f['food']
 
-        eda_data = urllib.request.urlopen(EDA_REQUEST).read()
+        eda_data = json.loads(urllib.request.urlopen(EDA_REQUEST).read().decode('utf-8'))
 
         fdata = {
             'dateTime': f['creation'],
@@ -33,5 +51,6 @@ def form():
             fdata['nutrients'] = [x * (0.5 * fdata['amount']) for x in fdata['nutrients']]
 
         print(fdata)
+        add_task(client, fdata['dateTime'], fdata)
 
     return render_template('index.html')
